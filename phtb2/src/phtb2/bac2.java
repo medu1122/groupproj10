25ee53a4eb5d7babc0057ddc2ae9a1522176ef4f
Tri Tran <vantriqn2212@gmail.com>
2024-04-03 19:09:56 +0700

    Bo sung code

--- phtb2/src/phtb2/bac2.java
+++ phtb2/src/phtb2/bac2.java
@@ -1,9 +1,45 @@
 package phtb2;
-
+import java.util.Scanner;
 public class bac2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		   
+    	System.out.println("Nhap so a :");
+    	int a =scanner.nextInt();
+    	System.out.println("Nhap so b :");
+    	int b =scanner.nextInt();
+    	System.out.println("Nhap so c :");
+    	int c =scanner.nextInt();
+    	
+    	if(a==0) {
+    		if(  b==0) {
+        		System.out.println("Phuong trinh vo so nghiem  !");
+        		
+        	}
+        	else if( b!=0) {
+        		System.out.println("Phuong trinh vo  nghiem !");
+        		
+        	}
+    	}
+    	else if(a!=0) {
+    	
+    	    int delta =b*b-4*a*c;
+    	    
+    	       if(delta<0) {
+    	    	   System.out.println("Phuong trinh vo nghiem");
+    	       }
+    	       else if(delta==0) {
+    	    	   System.out.println("Phuong trinh co nghiem kep ="+(-b)/2*a);
+    	       }
+    	       else {
+    	    	   double x1=(-b+(Math.sqrt(delta))/2*a);
+    	    	    double x2=(-b-(Math.sqrt(delta))/2*a);
+    	    	   System.out.println("Phuong trinh co nghiem x1 ="+x1);
+    	    	   System.out.println("Phuong trinh co nghiem x2 ="+x2);
+    	       }
+    	}
+	
 
 	}
 
